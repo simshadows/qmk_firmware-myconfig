32f23267d58b37569db187eb1397c184d0d4d828
simshadows <contact@simshadows.com>
2022-05-31 22:03:54 +1000

    Updated my 'idobao/montex/v1' keymap
    Also created relegendable keycaps for it!

--- keyboards/idobao/montex/v1/keymaps/simshadows/keymap.c
+++ keyboards/idobao/montex/v1/keymaps/simshadows/keymap.c
@@ -19,7 +19,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┌───┬───┬───┬───┐
-     * │ ` │Esc│Tab│MO1│Bsp│
+     * │MO1│Esc│Tab│MO1│Bsp│
      * ├───┼───┼───┼───┼───┤
      * │ , │Num│ / │ * │ - │
      * ├───┼───┼───┼───┼───┤
@@ -33,12 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───────┴───┴───┘
      */
     [0] = LAYOUT_numpad_6x5(
-        KC_GRAVE,  KC_ESC,  KC_TAB,  MO(1),   KC_BSPC,
-        KC_COMMA,  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_LGUI,   KC_P7,   KC_P8,   KC_P9,
-        KC_LALT,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-        KC_LSHIFT, KC_P1,   KC_P2,   KC_P3,
-        KC_LCTRL,  KC_P0,            KC_PDOT, KC_PENT
+        MO(1),       KC_MUTE, KC_VOLD, KC_VOLU, LAG(KC_F19),
+        LAG(KC_F18), KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        LAG(KC_F17), KC_P7,   KC_P8,   KC_P9,
+        LAG(KC_F16), KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        LAG(KC_F15), KC_P1,   KC_P2,   KC_P3,
+        LAG(KC_F14), KC_P0,            KC_PDOT, KC_PENT
     ),
 
     /*
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───────┴───┘───┘
      */
     [1] = LAYOUT_numpad_6x5(
-        QK_BOOT,   _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,
-        _______, KC_HOME, KC_UP,   KC_PGUP,
-        _______, KC_LEFT, XXXXXXX, KC_RGHT, _______,
-        _______, KC_END,  KC_DOWN, KC_PGDN,
-        _______, KC_INS,           KC_DEL,  _______
+        _______,     LSG(KC_F17), LSG(KC_F18), LSG(KC_F19), LAG(KC_F13),
+        LSG(KC_F16), LAG(KC_F21), LAG(KC_F22), LAG(KC_F24), LSG(KC_F24),
+        LSG(KC_F15), KC_HOME,     KC_UP,       KC_PGUP,
+        LSG(KC_F14), KC_LEFT,     XXXXXXX,     KC_RGHT,     _______,
+        LSG(KC_F13), KC_END,      KC_DOWN,     KC_PGDN,
+        LAG(KC_F20), KC_INS,                   KC_DEL,      QK_BOOT
     )
 };
